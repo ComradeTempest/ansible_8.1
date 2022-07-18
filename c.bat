2767638301f076f4bbc68e33fb4f6ae072319d7f
ComradeTempest <98019531+ComradeTempest@users.noreply.github.com>
2022-07-18 20:34:00 +0300

    Update c.bat

--- c.bat
+++ c.bat
@@ -1,9 +1,1 @@
-@echo off
-set FILES=main.go reader.go params.go stats.go defines.go requests.go formats.go
-
-if [%1] == [build] (
-	go build -o cdrsender.exe %FILES%
-) else (
-	start go run %FILES%
-)
 
