ef4d2d4589c02240cbc0285fdaf5e33b7a79eafa
ComradeTempest <98019531+ComradeTempest@users.noreply.github.com>
2022-07-18 20:33:36 +0300

    Update defines.go

--- defines.go
+++ defines.go
@@ -1,12 +1,1 @@
-package main
 
-const (
-	APP_NAME      = "cdrsender"
-	APP_VERSION   = "1.0.0.5"
-	INI_FILE_NAME = "cdrsender.ini"
-
-	URI_PATH_GETSTARTPOS = "/GetStartPosition"
-	URI_PATH_PROCESSCDR  = "/ProcessCdr"
-
-	JSON_CONTENT_TYPE = "application/json"
-)
